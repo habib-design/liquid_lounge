1a403879fe683aaee7ab3fb92de9895f993c9d02
Habibul Haque <sameerhaque0@gmail.com>
2026-09-01 16:19:38 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import gsap from "gsap";
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import { ScrollTrigger, SplitText } from "gsap/all"
+
+gsap.registerPlugin(ScrollTrigger, SplitText);
+
+const App = () => {
+  return (
+    <main>
+        <Navbar />
+        <Hero />
+        <div className="h-dvh bg-black" />
+    </main>
+  )
+}
+
+export default App
